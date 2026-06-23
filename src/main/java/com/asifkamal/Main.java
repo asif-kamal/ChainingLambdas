74002d0f604280977c7d1ccb3a8e59d7829fd844
asif-kamal <asif.kamal1489@gmail.com>
2026-06-23 17:19:06 -0500

    chaining lambda expressions

--- src/main/java/com/asifkamal/Main.java
+++ src/main/java/com/asifkamal/Main.java
@@ -1,17 +1,20 @@
 package com.asifkamal;
 
+import java.util.function.Function;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Function<String, String> uCase = String::toUpperCase;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Function<String, Integer> f2 = uCase
+                .andThen(s -> s.concat(" Buchalka"))
+                .andThen(s -> s.split(" "))
+                .andThen(s -> String.join(", ", s))
+                .andThen(String::length);
+        System.out.println(f2.apply("Tim"));
     }
 }
